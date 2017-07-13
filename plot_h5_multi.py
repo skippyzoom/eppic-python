3aaca29a2330513597a38f335bcb72f62efb70a9
Matt Young <may@bu.edu>
2017-07-12 21:20:45 -0400

    Cleaned up code. Plots transpose of input data. Added global colorbar.

--- plot_h5_multi.py
+++ plot_h5_multi.py
@@ -43,23 +43,6 @@ yf = int((3*eppic.ny/4)/eppic.nout_avg)
 ntMax = eppic_io.calc_timesteps(path)
 timeStep = [1,ntMax-1]
 
-# ## Loop over time steps and plot
-# cmap = plt.get_cmap(colorMap)
-# vmin = -0.06
-# vmax = 0.06
-# for ts in range(len(timeStep)):
-#     strStep = '{:06d}'.format(eppic.nout*timeStep[ts])
-#     fileName = 'parallel'+strStep+'.h5'
-#     plotName = dataName+'_'+strStep+'.'+plotType
-#     print("Reading",fileName)
-#     with h5py.File(path+'parallel/'+fileName,'r') as f:
-#         data = np.rot90(f['/'+dataName][int(x0):int(xf),int(y0):int(yf)],3)
-#     print("Plotting",plotName)
-#     plt.pcolormesh(data,cmap=cmap,vmin=vmin,vmax=vmax)
-#     plt.colorbar()
-#     plt.savefig(path+plotName)
-#     plt.close()
-
 ## Loop over time steps to read
 nX = int(eppic.nx*eppic.nsubdomains/eppic.nout_avg)
 nY = int(eppic.ny/eppic.nout_avg)
@@ -76,68 +59,37 @@ for it,ts in enumerate(timeStep):
     print("Reading",fileName)
     with h5py.File(path+'parallel/'+fileName,'r') as f:
         temp = np.array(f['/'+dataName])
-        # print(temp.shape)
-        # temp = np.rot90(temp,3)
-        # print(temp.shape)
-        # data[:,:,:,it] = temp.reshape(nX,nY,nZ)
+        # temp = np.transpose(temp)
         data[:,:,it] = temp
 
 xg = np.linspace(x0,xf,data[x0:xf,y0:yf,0].shape[0])
 yg = np.linspace(y0,yf,data[x0:xf,y0:yf,0].shape[1])
-
-# plt.pcolormesh(np.transpose(data[x0:xf,y0:yf,1]))
-# plt.xlim(x0, xf)
-# plt.ylim(y0, yf)
-# plt.gca().set_aspect('equal', adjustable='box')
-# plt.show()
-
-# f, axarr = plt.subplots(2)
-# axarr[0].pcolormesh(data[x0:xf,y0:yf,0])
-# axarr[0].set_title(strStep[0])
-# axarr[1].pcolormesh(data[x0:xf,y0:yf,1])
-# axarr[1].set_title(strStep[1])
-# # f.subplots_adjust(hspace=0.3)
-# plt.show()
-
-# plt.subplot(2,1,1)
-# plt.pcolormesh(xg,yg,data[x0:xf,y0:yf,0])
-# plt.axis([x0,xf,y0,yf])
-# plt.subplot(2,1,2)
-# plt.pcolormesh(xg,yg,data[x0:xf,y0:yf,1])
-# plt.axis([x0,xf,y0,yf])
-# plt.show()
+# print(data[x0:xf,y0:yf,0].shape[0])
+# print(data[x0:xf,y0:yf,0].shape[1])
 
 dataMaxAbs = np.nanmax(np.absolute(data))
-# vmin = data.min()
-# vmax = data.max()
+# vmin = data.nanmin()
+# vmax = data.nanmax()
 vmin = -dataMaxAbs
 vmax = dataMaxAbs
-print(vmin,vmax)
+# print(vmin,vmax)
 cmap = plt.get_cmap(colorMap)
 fig, axes = plt.subplots(2, sharex=True)
-img = axes[0].pcolormesh(xg,yg,data[x0:xf,y0:yf,0],
+img = axes[0].pcolormesh(xg,yg,
+                         np.transpose(data[x0:xf,y0:yf,0]),
                          cmap=cmap,vmin=vmin,vmax=vmax)
 axes[0].set_title(strStep[0])
-img = axes[1].pcolormesh(xg,yg,data[x0:xf,y0:yf,1],
+axes[0].set_aspect('equal')
+img = axes[1].pcolormesh(xg,yg,
+                         np.transpose(data[x0:xf,y0:yf,1]),
                          cmap=cmap,vmin=vmin,vmax=vmax)
 axes[1].set_title(strStep[1])
+axes[1].set_aspect('equal')
 fig.suptitle('Density')
-# fig.text(0.50, 0.95, 'Density', transform=fig.transFigure, horizontalalignment='center')
 fig.subplots_adjust(right=0.80)
 cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.70])
-fig.colorbar(img,cax=cbar_ax)
+cbar = fig.colorbar(img,cax=cbar_ax)
+cbar.set_label('$\delta n/n_0$')
 plt.show()
-
-# ## The following requires from matplotlib.pyplot import figure, show
-# xg = np.linspace(x0,xf,data[:,:,0].shape[0])
-# yg = np.linspace(y0,yf,data[:,:,0].shape[1])
-# fg = figure()
-# ax = fg.gca()
-# pc = ax.pcolormesh(xg,yg,data[:,:,1])
-# ax.set_xlabel('x [m]')
-# ax.set_ylabel('z [m]')
-# ax.set_title('Density')
-# fg.colorbar(pc).set_label('$\delta n/n_0$')
-
 
 print("\n")
