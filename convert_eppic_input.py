08f38e276062ab47a5b681437c9dcbebb3bd2309
Matt Young <may@bu.edu>
2017-07-13 11:32:20 -0400

    Use os.path.join to create search path.

--- convert_eppic_input.py
+++ convert_eppic_input.py
@@ -5,8 +5,10 @@
 import fnmatch, os, eppic_io
 
 baseDir = '/projectnb/eregion/may/Stampede_runs/'
+projDir = 'parametric_wave'
+path = os.path.join(baseDir,projDir)
 targets = []
-for root, dirs, files in os.walk(baseDir):
+for root, dirs, files in os.walk(path):
     for fn in files:
         if fnmatch.fnmatch(fn,"eppic.i"):
             eppic_io.clean_params(root)
